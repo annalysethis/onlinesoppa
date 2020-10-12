25fcbfa424eae0048444f680582f5b9b4762293e
annalysethis <annalys01@gmail.com>
2020-10-12 09:32:18 +0200

    started on a filter function

--- src/components/FoodFilter/FoodFilter.js
+++ src/components/FoodFilter/FoodFilter.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 import * as styled from "./FoodFilter.styled";
 
-export class FoodFilter extends Component {
-  constructor(props) {
-    super(props);
+// export class FoodFilter extends Component {
 
-    this.state = {
-      show: null,
-    };
-  }
-  render() {
-    return (
+//   render() {
+//     return (
+//       <styled.BtnContainer>
+//         <styled.Button >Kött</styled.Button>
+//         <styled.Button>Kyckling</styled.Button>
+//         <styled.Button>Fisk</styled.Button>
+//         <styled.Button>Vegansk</styled.Button>
+//       </styled.BtnContainer>
+//     );
+//   }
+// }
+
+// export default FoodFilter;
+
+export default function FoodFilter(props) {
+  return (
+    <React.Fragment>
       <styled.BtnContainer>
-        <styled.Button onClick="show">Kött</styled.Button>
+        <styled.Button value="Kött">Kött</styled.Button>
         <styled.Button>Kyckling</styled.Button>
         <styled.Button>Fisk</styled.Button>
         <styled.Button>Vegansk</styled.Button>
       </styled.BtnContainer>
-    );
-  }
+    </React.Fragment>
+  );
 }
-
-export default FoodFilter;
--- src/components/SoupMenu/SoupMenu.js
+++ src/components/SoupMenu/SoupMenu.js
@@ -1,31 +1,72 @@
-import React, { Component } from "react";
+import React from "react";
 import SoupData from "./../Data/Soups.json";
 import * as styled from "./SoupMenu.styled";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "./../Navbar";
 import FoodFilter from "./../FoodFilter";
 
-class SoupMenu extends Component {
-  render() {
-    return (
-      <div>
-        {/* <Location /> */}
-        <styled.AdressWrapper>
-          <styled.StyledUserIcon icon={faMapMarkerAlt} />
-          <styled.Location>
-            {this.props.location.state && this.props.location.state.adress}
-          </styled.Location>
-        </styled.AdressWrapper>
-        <Navbar />
-        <FoodFilter />
-        {SoupData.map((item, index) => {
-          return (
+// class SoupMenu extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* <Location /> */}
+//         <styled.AdressWrapper>
+//           <styled.StyledUserIcon icon={faMapMarkerAlt} />
+//           <styled.Location>
+//             {this.props.location.state && this.props.location.state.adress}
+//           </styled.Location>
+//         </styled.AdressWrapper>
+//         <Navbar />
+//         <FoodFilter />
+//         {SoupData.map((item, index) => {
+//           return (
+//             <styled.Container key={index} id={item.id}>
+//               <styled.ImgWrapper>
+//                 <styled.Image src={item.image}></styled.Image>
+//               </styled.ImgWrapper>
+//               <styled.Wrapper>
+//                 <styled.SoupType>{item.category}</styled.SoupType>
+
+//                 <styled.SoupTitle>{item.title}</styled.SoupTitle>
+//                 <styled.Info>{item.info}</styled.Info>
+//                 <styled.IngrediensHeader>INGREDIENSER:</styled.IngrediensHeader>
+//                 <styled.IngrediensWrapper>
+//                   <styled.Ingrediens>{item.ing1}</styled.Ingrediens>
+//                   <styled.Ingrediens>{item.ing2}</styled.Ingrediens>
+//                   <styled.Ingrediens>{item.ing3}</styled.Ingrediens>
+//                   <styled.Ingrediens>{item.ing4}</styled.Ingrediens>
+//                 </styled.IngrediensWrapper>
+//               </styled.Wrapper>
+//             </styled.Container>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// export default SoupMenu;
+
+export default function SoupMenu(props) {
+  return (
+    <div>
+      <styled.AdressWrapper>
+        <styled.StyledUserIcon icon={faMapMarkerAlt} />
+        <styled.Location>
+          {props.location.state && props.location.state.adress}
+        </styled.Location>
+      </styled.AdressWrapper>
+      <Navbar />
+      <FoodFilter />
+      {SoupData.map((item, index) => {
+        return (
+          <React.Fragment>
             <styled.Container key={index} id={item.id}>
               <styled.ImgWrapper>
                 <styled.Image src={item.image}></styled.Image>
               </styled.ImgWrapper>
               <styled.Wrapper>
-                <styled.SoupType>{item.type}</styled.SoupType>
+                <styled.SoupType>{item.category}</styled.SoupType>
 
                 <styled.SoupTitle>{item.title}</styled.SoupTitle>
                 <styled.Info>{item.info}</styled.Info>
@@ -38,11 +79,9 @@ class SoupMenu extends Component {
                 </styled.IngrediensWrapper>
               </styled.Wrapper>
             </styled.Container>
-          );
-        })}
-      </div>
-    );
-  }
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
 }
-
-export default SoupMenu;
